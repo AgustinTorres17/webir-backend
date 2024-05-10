70c7c6a7ce2a224cd33a6b842ac821105ab8ccdc
agustin.torres <agustintorres1701@gmail.com>
2024-05-09 23:44:52 -0300

    back init

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,59 @@
-const { Router } = require('express');
-
+const { Router } = require("express");
+const axios = require("axios");
 const router = Router();
 
-router.get('/movie:id', (req, res) => {
-    res.send('Movie id: ' + req.params.id);
-})
+router.get("/", async (req, res) => {
+  res.send("Working");
+});
 
+router.get("/movies", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-ES&page=1&sort_by=popularity.desc",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmUzOTliYjZmZDY0NDMxYjNiYmUzNThiODUyODRjNyIsInN1YiI6IjY1OTA4ZDI2Y2U0ZGRjNmVkNTdkNWM2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hDyxnpPH2gk96U1Kl_8-53fAI5L47FiqJwjYzDyiqio",
+    },
+  };
+
+  const response = await axios.request(options);
+  res.json(response.data);
+});
+
+router.get("/series", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=false&language=es-ES&page=1&sort_by=popularity.desc",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmUzOTliYjZmZDY0NDMxYjNiYmUzNThiODUyODRjNyIsInN1YiI6IjY1OTA4ZDI2Y2U0ZGRjNmVkNTdkNWM2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hDyxnpPH2gk96U1Kl_8-53fAI5L47FiqJwjYzDyiqio",
+    },
+  };
+
+  const response = await axios.request(options);
+  const series = response.data.results.map((series) => ({
+    ...series,
+    release_date: series.first_air_date,
+    title: series.name,
+  }));
+  res.json({ ...response.data, results: series });
+});
+
+router.get("/genres", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/genre/movie/list?language=es-ES`,
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmUzOTliYjZmZDY0NDMxYjNiYmUzNThiODUyODRjNyIsInN1YiI6IjY1OTA4ZDI2Y2U0ZGRjNmVkNTdkNWM2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hDyxnpPH2gk96U1Kl_8-53fAI5L47FiqJwjYzDyiqio",
+    },
+  };
+
+  const response = await axios.request(options);
+  res.json(response.data);
+});
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,17 +3,12 @@ const morgan = require("morgan");
 const app = express();
 const taskRoutes = require("./routes/task.routes");
 
+const cors = require("cors");
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(taskRoutes);
 
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
 app.listen(3000);
-
-
